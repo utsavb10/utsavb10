5ada783a95168cc3b01fe26c744a7991aa5d77bf
Utsav <utsav@test.com>
2020-03-30 23:36:56 +0530

    Initial commit

---
+++ src/main/java/com/sample/validation/validation/ValidationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sample.validation.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/validation/validation/ValidationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.validation.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
